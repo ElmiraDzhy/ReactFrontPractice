1fe984c213dc8194cd67242852be62b5c90ee83b
ElyaDzhy <elmira.freshcode@gmail.com>
2023-05-09 15:28:33 +0300

    Refactor

--- client/src/components/InputComponents/ImageUpload/ImageUpload.js
+++ client/src/components/InputComponents/ImageUpload/ImageUpload.js
@@ -1,46 +1,47 @@
-import React, { useEffect, useRef } from 'react';
-import classNames from 'classnames';
-import { useField } from 'formik';
+import React, { useRef } from "react";
+import { useField } from "formik";
 
 const ImageUpload = (props) => {
-  const [field, meta, helpers] = useField(props.name);
-  const { uploadContainer, inputContainer, imgStyle } = props.classes;
+	const [field] = useField(props.name);
+	const { uploadContainer, inputContainer, imgStyle } = props.classes;
+	const imgRef = useRef(null);
 
-  const imgRef = useRef( null );
+  const reader = new FileReader();
+  reader.onload = () => {
+    imgRef.current.src = reader.result;
+  };
 
-  const onChange = ( e ) => {
-    const file = e.target.files[0];
-    const imageTypeRegexp = /image.*/;
-    if ( !file.type.match( imageTypeRegexp ) ) {
-      e.target.value = '';
-    } else {
-      field.onChange( file.type );
-      const reader = new FileReader();
-      reader.onload = () => {
-         imgRef.current.src = reader.result;
-      }
-      reader.readAsDataURL(file);
-    }
-  }
+	const onChange = (e) => {
+		const file = e.target.files[0];
+		const imageTypeRegexp = /image.*/;
+		if (!file.type.match(imageTypeRegexp)) {
+			e.target.value = "";
+		} else {
+			reader.readAsDataURL(file);
+		}
+	};
 
-  return (
-    <div className={uploadContainer}>
-      <div className={inputContainer}>
-        <span>Support only images (*.png, *.gif, *.jpeg)</span>
-        <input
-          {...field}
-          id="fileInput"
-          type="file"
-          accept=".jpg, .png, .jpeg"
-          onChange={onChange}
-        />
-        <label htmlFor="fileInput">Chose file</label>
-      </div>
-      <img className={imgStyle} ref={ imgRef } alt="user"  />
-      
-     
-    </div>
-  );
+	return (
+		<div className={uploadContainer}>
+			<div className={inputContainer}>
+				<span>Support only images (*.png, *.gif, *.jpeg)</span>
+				<input
+					{...field}
+					id="fileInput"
+					type="file"
+					accept=".jpg, .png, .jpeg"
+					onChange={onChange}
+				/>
+				<label htmlFor="fileInput">Chose file</label>
+			</div>
+			<img
+				className={imgStyle}
+				ref={imgRef}
+				alt="user"
+			/>
+		</div>
+	);
 };
 
 export default ImageUpload;
+
